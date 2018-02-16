3688e02bdbbddda2423c120ceed7b273af56723e
Leigh Forrest <leighmforrest@comcast.net>
2018-02-15 19:26:02 -0500

    starting users

--- feelings/settings.py
+++ feelings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'thoughts'
 ]
 
@@ -74,12 +75,6 @@ WSGI_APPLICATION = 'feelings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 
@@ -121,3 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+
+
+# Authentication
+LOGIN_URL = 'users:login'
