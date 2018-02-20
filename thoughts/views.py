59b936c31730367b3edfd9165b14d3f42189ab3a
Leigh Forrest <leighmforrest@comcast.net>
2018-02-19 21:50:37 -0500

    saving a thought works on dashboard

--- thoughts/views.py
+++ thoughts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+from .forms import ThoughtForm
 
-# Create your views here.
+class CreateThought(LoginRequiredMixin, CreateView):
+    form_class = ThoughtForm
+    success_url =  reverse_lazy('users:dashboard')
+
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
